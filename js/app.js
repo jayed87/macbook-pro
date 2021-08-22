4280194845f22717f998ad41200a6d93f1b8c961
JayedAkbarSumon <akbarjayed87@gmail.com>
2021-08-22 15:44:49 +0600

    compled

--- js/app.js
+++ js/app.js
@@ -5,50 +5,64 @@ function Price(id) {
     return productPrice;
 }
 // vat
-// function vat(id) {
-//     const =
-// }
+function amountField(id) {
+    const field = document.getElementById(id);
+    return field;
+}
 // memory option 8GB
 document.getElementById('memory-8GB').addEventListener('click', function () {
-    const extMemoryCost = document.getElementById('extra-memory-cost');
+    const extMemoryCost = amountField('extra-memory-cost');
     extMemoryCost.innerText = 0;
     const extStoragePrice = Price('extra-storage-cost');
     const deliveryPrice = Price('deliver-field');
-    const totalPriceField = document.getElementById('total-price');
+    const totalPriceField = amountField('total-price');
+    const finalPriceField = amountField('final-price');
     totalPriceField.innerText = 1299 + extStoragePrice + deliveryPrice;
-    // console.log(totalPrice);
+    finalPriceField.innerText = 1299 + extStoragePrice + deliveryPrice;
 });
 // memory option 16GB
 document.getElementById('memory-16GB').addEventListener('click', function () {
-    const extMemoryCostId = document.getElementById('extra-memory-cost');
+    const extMemoryCostId = amountField('extra-memory-cost');
     extMemoryCostId.innerText = 180;
-    const basePriceText = document.getElementById('base-price');
-    const basePrice = parseFloat(basePriceText.innerText);
-    const totalPriceText = document.getElementById('total-price');
-    totalPriceText.innerText = 180 + basePrice;
+    const extStoragePrice = Price('extra-storage-cost');
+    const deliveryPrice = Price('deliver-field');
+    const totalPriceField = amountField('total-price');
+    const finalPriceField = amountField('final-price');
+    totalPriceField.innerText = 1299 + extStoragePrice + deliveryPrice + 180;
+    finalPriceField.innerText = 1299 + extStoragePrice + deliveryPrice + 180;
 });
 // storage option 256GB
 document.getElementById('storage-256GB').addEventListener('click', function () {
-    const extStorageCost = document.getElementById('extra-storage-cost');
+    const extStorageCost = amountField('extra-storage-cost');
     extStorageCost.innerText = 0;
+    const extMemoryPrice = Price('extra-memory-cost');
+    const deliveryPrice = Price('deliver-field');
+    const totalPriceField = amountField('total-price');
+    const finalPriceField = amountField('final-price');
+    totalPriceField.innerText = 1299 + deliveryPrice + extMemoryPrice + 0;
+    finalPriceField.innerText = 1299 + deliveryPrice + extMemoryPrice + 0;
 });
 // storage option 512GB
 document.getElementById('storage-512GB').addEventListener('click', function () {
-    const extStorageCost = document.getElementById('extra-storage-cost');
+    const extStorageCost = amountField('extra-storage-cost');
     extStorageCost.innerText = 100;
-    const extMemoryCostId = document.getElementById('extra-memory-cost');
-    const extMemoryPrice = parseFloat(extMemoryCostId.innerText);
-    const totalPriceText = document.getElementById('total-price');
-    totalPriceText.innerText = 100 + 1299 + extMemoryPrice;
+    const extMemoryCost = Price('extra-memory-cost');
+    const totalPriceText = amountField('total-price');
+    const deliveryPrice = Price('deliver-field');
+    const finalPriceField = amountField('final-price');
+    totalPriceText.innerText = 100 + 1299 + extMemoryCost + deliveryPrice;
+    finalPriceField.innerText = 100 + 1299 + extMemoryCost + deliveryPrice;
 });
 // storage option 1T
 document.getElementById('storage-1T').addEventListener('click', function () {
-    const extStorageCost = document.getElementById('extra-storage-cost');
+    const extStorageCost = amountField('extra-storage-cost');
     extStorageCost.innerText = 180;
-    const extMemoryCostId = document.getElementById('extra-memory-cost');
-    const extMemoryPrice = parseFloat(extMemoryCostId.innerText);
-    const totalPriceText = document.getElementById('total-price');
-    totalPriceText.innerText = 180 + 1299 + extMemoryPrice;
+    const extMemoryCost = Price('extra-memory-cost');
+    const totalPriceText = amountField('total-price');
+    const deliveryPrice = Price('deliver-field');
+    const finalPriceField = amountField('final-price');
+    totalPriceText.innerText = 180 + 1299 + extMemoryCost + deliveryPrice;
+    finalPriceField.innerText = 180 + 1299 + extMemoryCost + deliveryPrice;
 });
 // delivery option $20
 document.getElementById('delivery-cost').addEventListener('click', function () {
@@ -56,8 +70,10 @@ document.getElementById('delivery-cost').addEventListener('click', function () {
     extDeliveryCost.innerText = 20;
     const extStoragePrice = Price('extra-storage-cost');
     const extMemoryPrice = Price('extra-memory-cost');
-    const totalPriceField = document.getElementById('total-price');
+    const totalPriceField = amountField('total-price');
+    const finalPriceField = amountField('final-price');
     totalPriceField.innerText = 1299 + extStoragePrice + extMemoryPrice + 20;
+    finalPriceField.innerText = 1299 + extStoragePrice + extMemoryPrice + 20;
 });
 // delivery option free
 document.getElementById('delivery-free').addEventListener('click', function () {
@@ -65,6 +81,21 @@ document.getElementById('delivery-free').addEventListener('click', function () {
     extDeliveryCost.innerText = 0;
     const extStoragePrice = Price('extra-storage-cost');
     const extMemoryPrice = Price('extra-memory-cost');
-    const totalPriceField = document.getElementById('total-price');
+    const totalPriceField = amountField('total-price');
+    const finalPriceField = amountField('final-price');
     totalPriceField.innerText = 1299 + extStoragePrice + extMemoryPrice;
+    finalPriceField.innerText = 1299 + extStoragePrice + extMemoryPrice;
+});
+
+// vat apply 
+
+document.getElementById('code-apply-btn').addEventListener('click', function () {
+    const totalPrice = Price('total-price');
+    const codeInput = document.getElementById('code-input').value;
+    const totalPriceField = amountField('final-price');
+    if (codeInput == 'stevekaku') {
+        const priceAfterDiscount = totalPrice - ((totalPrice * 20) / 100);
+        totalPriceField.innerText = priceAfterDiscount;
+    }
+
 });
